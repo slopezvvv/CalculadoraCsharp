0fce992e75989bb81fd51b55cc10e2b40d01e354
yo <yo@DESKTOP-RVOAHLO>
2020-04-13 19:45:38 -0400

    initial commit

---
+++ Aritmetica.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculadora
+{
+    class Aritmetica
+    {
+        public int Suma(int a, int b)
+        {
+            return a+b;
+        }
+        public int Resta(int a, int b)
+        {
+            return a-b;
+        }
+        public int Multi(int a, int b)
+        {
+            return a*b;
+        }
+
+        public int Divi(double tmpDividendo, double tmpDivisor)
+        {
+            int divisor = int.Parse(Math.Round(tmpDivisor).ToString());
+            int dividendo = int.Parse(Math.Round(tmpDividendo).ToString());
+            if (divisor == 0) {
+                Console.WriteLine("La division por cero no es valida.");
+                return 0;
+            }
+
+            if (dividendo == 0)
+                return 0;
+
+            return dividendo/divisor;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        private static Aritmetica _aritmetica = new Aritmetica();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido a la Calculadora:");
+            Console.WriteLine("1. Sumar");
+            Console.WriteLine("2. Restar");
+            Console.WriteLine("3. Multiplicar");
+            Console.WriteLine("4. Dividir");
+            Console.WriteLine("0. Salir");
+            string input = Console.ReadLine().ToLower().Trim();
+            try
+            {
+                int intInput = int.Parse(input);
+                int a = 0, b = 0;
+                switch (intInput)
+                {
+                    case 1:
+                        Console.WriteLine("Ingrese el primer factor:");
+                        a = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingrese el segundo factor:");
+                        b = int.Parse(Console.ReadLine());
+                        Console.WriteLine("El resultado es: "+_aritmetica.Suma(a, b));
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("No se puede leer el input porque no es un numero:");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
